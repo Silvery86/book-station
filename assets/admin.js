5e24464c6ac04e0465acf6e6416c0df248ea6d64
giang <giang8692@gmail.com>
2024-12-09 10:40:08 +0700

    Add: Layout and dashboard for admin

---
+++ assets/admin.js
@@ -0,0 +1,39 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * This file will be included onto the page via the importmap() Twig function,
+ * which should already be in your base.html.twig.
+ */
+import './admin/vendor/fonts/fontawesome.css'
+import './admin/vendor/fonts/tabler-icons.css'
+import './admin/vendor/fonts/flag-icons.css'
+import './admin/vendor/css/rtl/core.css'
+import './admin/vendor/css/rtl/theme-default.css'
+import './admin/css/demo.css'
+import './admin/vendor/libs/node-waves/node-waves.css'
+import './admin/vendor/libs/perfect-scrollbar/perfect-scrollbar.css'
+import './admin/vendor/libs/typeahead-js/typeahead.css'
+import './admin/vendor/libs/apex-charts/apex-charts.css'
+import './admin/vendor/libs/swiper/swiper.css'
+import './admin/vendor/libs/datatables-bs5/datatables.bootstrap5.css'
+import './admin/vendor/libs/datatables-responsive-bs5/responsive.bootstrap5.css'
+import './admin/vendor/libs/datatables-checkboxes-jquery/datatables.checkboxes.css'
+import './admin/vendor/css/pages/cards-advance.css'
+
+
+import './admin/vendor/js/helpers.js'
+import './admin/js/config.js'
+import './admin/vendor/libs/jquery/jquery.js'
+import './admin/vendor/libs/popper/popper.js'
+import './admin/vendor/js/bootstrap.js'
+import './admin/vendor/libs/node-waves/node-waves.js'
+import './admin/vendor/libs/perfect-scrollbar/perfect-scrollbar.js'
+import './admin/vendor/libs/hammer/hammer.js'
+import './admin/vendor/libs/i18n/i18n.js'
+import './admin/vendor/libs/typeahead-js/typeahead.js'
+import './admin/vendor/js/menu.js'
+import './admin/vendor/libs/apex-charts/apexcharts.js'
+import './admin/vendor/libs/swiper/swiper.js'
+import './admin/vendor/libs/datatables-bs5/datatables-bootstrap5.js'
+import './admin/js/main.js'
+import './admin/js/dashboards-analytics.js'
