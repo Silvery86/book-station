6f407f15cb098a00e6e701a13a9b0d93af05874b
giang <giang8692@gmail.com>
2024-12-09 08:44:58 +0700

    Update: Update assets file

---
+++ src/DataFixtures/User.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Composer\XdebugHandler\Status;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class User extends Fixture
+{
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        // Create a default admin user
+        $admin = new \App\Entity\User();
+        $admin->setEmail('admin@gmail.com');
+        $admin->setUsername('admin');
+
+        // Hash the password
+        $hashedPassword = $this->passwordHasher->hashPassword($admin, 'admin@123');
+        $admin->setPassword($hashedPassword);
+        // Set roles
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setStatus(\App\Entity\User::STATUS_ACTIVE);
+        // Persist the user
+        $manager->persist($admin);
+
+        $manager->flush();
+    }
+
+
+}
