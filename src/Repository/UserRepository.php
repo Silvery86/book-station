b93fbea82b2be7e520ea6777165ffe7c359151f8
giang <giang8692@gmail.com>
2024-12-09 21:14:00 +0700

    Update: Login for admin, remember me option, login use username and email

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -5,24 +5,25 @@ namespace App\Repository;
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
 
-/**
- * @extends ServiceEntityRepository<User>
- */
-class UserRepository extends ServiceEntityRepository
+class UserRepository extends ServiceEntityRepository implements UserLoaderInterface
 {
     public function __construct(ManagerRegistry $registry)
     {
-        parent::__construct($registry, User::class);
+        parent::__construct($registry, User::class); // Pass User::class here
     }
-
-    public function loadUserByIdentifier(string $identifier): ?User
+    public function loadUserByIdentifier(string $usernameOrEmail): ?User
     {
-        // Try to find the user by username or email
-        return $this->createQueryBuilder('u')
-            ->where('u.username = :identifier OR u.email = :identifier')
-            ->setParameter('identifier', $identifier)
-            ->getQuery()
+        $entityManager = $this->getEntityManager();
+
+        return $entityManager->createQuery(
+            'SELECT u
+                FROM App\Entity\User u
+                WHERE u.username = :query
+                OR u.email = :query'
+        )
+            ->setParameter('query', $usernameOrEmail)
             ->getOneOrNullResult();
     }
 }
--- src/Security/UserChecker.php
+++ src/Security/UserChecker.php
@@ -3,12 +3,19 @@
 namespace App\Security;
 
 use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Security\Core\Exception\CustomUserMessageAccountStatusException;
 use Symfony\Component\Security\Core\User\UserCheckerInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 class UserChecker implements UserCheckerInterface
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
     public function checkPreAuth(UserInterface $user): void
     {
         if (!$user instanceof User) {
@@ -34,6 +41,13 @@ class UserChecker implements UserCheckerInterface
 
     public function checkPostAuth(UserInterface $user): void
     {
+        if (!$user instanceof User) {
+            return;
+        }
 
+        // Update the last_login_at timestamp after a successful login
+        $user->setLastLoginAt(new \DateTime());
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
     }
 }
