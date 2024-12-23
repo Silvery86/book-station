d895dd5f83a40a5ec7665edc60ff0ca9ecc02d05
giang <giang8692@gmail.com>
2024-12-23 09:55:10 +0700

    Update: Update home controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,8 +11,9 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        $title = "Home Page";
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'title' => $title,
         ]);
     }
 }
